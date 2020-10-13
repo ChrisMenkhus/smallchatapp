50a91169c339f57cc60d97074894dce914e11db2
Chris <chrismenkhus.dev@gmail.com>
2020-10-12 22:06:39 -0600

    Updates

---
+++ precache-manifest.9c23d3542a0fadf1985eb5b988fb0df9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca8314c5da611dc621fd8e5b3df2caa5",
+    "url": "/smallchatapp/index.html"
+  },
+  {
+    "revision": "0d872b72c375dcff8908",
+    "url": "/smallchatapp/static/css/main.e7583bce.chunk.css"
+  },
+  {
+    "revision": "a8a053d0ba8f3b308d3f",
+    "url": "/smallchatapp/static/js/2.6d053f0d.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/smallchatapp/static/js/2.6d053f0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d872b72c375dcff8908",
+    "url": "/smallchatapp/static/js/main.4ad0a955.chunk.js"
+  },
+  {
+    "revision": "0cc806af70d2fa3f1278",
+    "url": "/smallchatapp/static/js/runtime-main.02d17ba5.js"
+  }
+]);
